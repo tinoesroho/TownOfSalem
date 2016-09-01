ff2ad1cf65f62163416926604694cb1d45190a6a
Dreamiko <haleem14789@gmail.com>
2016-09-01 18:12:23 -0300

    Init
    Init

---
+++ js/javascript.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+	
+    $("#button0").click(function(){
+     
+        if($(this).is(":checked")){
+            $("#tester").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(1)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+      $("#button1").click(function(){
+        if($(this).is(":checked")){
+            $(".greenColor:nth-child(2)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(2)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+       $("#button2").click(function(){
+        if($(this).is(":checked")){
+            $(".greenColor:nth-child(3)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(3)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+        $("#button3").click(function(){
+        if($(this).is(":checked")){
+            $(".greenColor:nth-child(4)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(4)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+          $("#button4").click(function(){
+        if($(this).is(":checked")){
+            $(".greenColor:nth-child(5)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(5)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+              $("#button5").click(function(){
+        if($(this).is(":checked")){
+            $(".greenColor:nth-child(6)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(6)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+            $("#button6").click(function(){
+        if($(this).is(":checked")){
+            $(".greenColor:nth-child(7)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".greenColor:nth-child(7)").css({'color': '#7FF308','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+            $("#button7").click(function(){
+        if($(this).is(":checked")){
+            $(".blueColor:nth-child(8)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".blueColor:nth-child(8)").css({'color': 'rgb(66, 192, 251)','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+          $("#button8").click(function(){
+        if($(this).is(":checked")){
+            $(".redColor:nth-child(9)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".redColor:nth-child(9)").css({'color': '#DD0000','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+        $("#button9").click(function(){
+        if($(this).is(":checked")){
+            $(".redColor:nth-child(10)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".redColor:nth-child(10)").css({'color': '#DD0000','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+    $("#button10").click(function(){
+        if($(this).is(":checked")){
+            $(".blueColor:nth-child(11)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".blueColor:nth-child(11)").css({'color': 'rgb(66, 192, 251)','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+       $("#button11").click(function(){
+        if($(this).is(":checked")){
+            $(".grayColor:nth-child(12)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".grayColor:nth-child(12)").css({'color': '#C0BFBE','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+      $("#button12").click(function(){
+        if($(this).is(":checked")){
+            $(".grayColor:nth-child(13)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".grayColor:nth-child(13)").css({'color': '#C0BFBE','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+      $("#button13").click(function(){
+        if($(this).is(":checked")){
+            $(".grayColor:nth-child(14)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".grayColor:nth-child(14)").css({'color': '#C0BFBE','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+        $("#button14").click(function(){
+        if($(this).is(":checked")){
+            $(".whiteColor:nth-child(15)").css({'color': 'red','text-transform': 'uppercase'
+                                 ,'text-decoration': 'line-through'});
+        } else {
+            $(".whiteColor:nth-child(15)").css({'color': 'white','text-transform': 'capitalize', 'text-decoration': 'none'});
+        }
+    });
+    
+    
+
+});
